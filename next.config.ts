1063c2d42f447ab8564663cb7875c68221371388
Nguyễn Tuấn Sơn <11215179@st.neu.edu.vn>
2025-04-13 21:44:15 +0700

    Fix BundleAnalyzer ESLint bug

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true',
-});
-
 const nextConfig: NextConfig = {
     output: 'standalone',
     images: {
@@ -21,4 +16,4 @@ const nextConfig: NextConfig = {
     trailingSlash: true,
 };
 
-module.exports = withBundleAnalyzer(nextConfig);
+module.exports = nextConfig;
