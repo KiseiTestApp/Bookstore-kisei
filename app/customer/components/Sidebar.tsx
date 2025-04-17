b382a2165f5937c8d0be2a576e94b1757ea061c9
KiseiTestApp <tuanson.dev.test@gmail.com>
2025-04-17 10:23:41 +0700

    Make review form properly closed when user submitted form now

--- app/customer/components/Sidebar.tsx
+++ app/customer/components/Sidebar.tsx
@@ -10,17 +10,17 @@ export default function CustomerSidebar() {
     const theme = useTheme();
 
     const navItems = [
-        { name: 'Hồ sơ cá nhân', href: '/customer/account/' },
-        { name: 'Sổ địa chỉ', href: '/customer/address/' },
-        { name: 'Thay đổi mật khẩu', href: '/customer/change-password/' },
-        { name: 'Lịch sử mua hàng', href: '/customer/order-history/' },
+        { name: 'Hồ sơ cá nhân', href: '/customer/account' },
+        { name: 'Sổ địa chỉ', href: '/customer/address' },
+        { name: 'Thay đổi mật khẩu', href: '/customer/change-password' },
+        { name: 'Lịch sử mua hàng', href: '/customer/order-history' },
     ];
 
     return (
         <List sx={{ width: 250 }}>
             {navItems.map((item) => (
                 <ListItem key={item.href} disablePadding>
-                    <Link href={item.href} passHref legacyBehavior>
+                    <Link href={item.href} passHref>
                         <ListItemButton
                             selected={pathname.startsWith(item.href)}
                             sx={{
